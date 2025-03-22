0105f8fc7d2243be5807df57015357428ded8d61
Kairo <70084790+ygypt@users.noreply.github.com>
2025-03-22 18:49:55 -0400

    fulscreen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
 use iocraft::prelude::*;
 
 fn main() {
+    smol::block_on(element!(Mainview).render_loop()).unwrap();
+    //element!(Lilview).render_loop().await.unwrap();
+}
+
+#[component]
+fn Mainview(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    let (width, height) = hooks.use_terminal_size();
+    let mut should_exit = hooks.use_state(|| false);
+    let mut system = hooks.use_context_mut::<SystemContext>();
+
+    hooks.use_terminal_events({
+        move |event| match event {
+            TerminalEvent::Key(KeyEvent { code, kind, .. }) if kind != KeyEventKind::Release => {
+                match code {
+                    KeyCode::Char('q') => should_exit.set(true),
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    });
+
+    if should_exit.get() {
+        system.exit();
+    }
+
+    element! {
+        View(
+            width: width - 1,
+            height,
+            background_color: Color::DarkGrey,
+            border_style: BorderStyle::Single,
+            border_color: Color::Grey,
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+        ){
+            Lilview()
+            Text(content: "Press q to quit")
+        }
+    }
+}
+
+#[component]
+fn Lilview(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
     element! {
         View(
             border_style: BorderStyle::Round,
@@ -9,5 +54,4 @@ fn main() {
             Text(content: "Hello, world!")
         }
     }
-    .print();
 }
