c5cf14d60675c10219270cbecd0f3beed5d10a0e
Kairo <70084790+ygypt@users.noreply.github.com>
2025-03-22 22:07:48 -0400

    primitive multiline text input. only backspacing though

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 use iocraft::prelude::*;
 
+/*
+* Basically two modes:
+*   Edit
+*   Explore
+* Use Ctrl+E to swap between the list and the editor
+* 
+* Tree
+*   App:
+*       NoteEditor:
+*           TextInput
+*           Controls
+*       NoteExplorer:
+*           NoteList:
+*               Note, Note, etc
+*           Controls
+*/
+
 fn main() {
-    smol::block_on(element!(Mainview).render_loop()).unwrap();
+    smol::block_on(element!(App).render_loop()).unwrap();
     //element!(Lilview).render_loop().await.unwrap();
 }
 
 #[component]
-fn Mainview(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+fn App(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
     let (width, height) = hooks.use_terminal_size();
     let mut should_exit = hooks.use_state(|| false);
     let mut system = hooks.use_context_mut::<SystemContext>();
 
     hooks.use_terminal_events({
         move |event| match event {
-            TerminalEvent::Key(KeyEvent { code, kind, .. }) if kind != KeyEventKind::Release => {
-                match code {
-                    KeyCode::Char('q') => should_exit.set(true),
+            TerminalEvent::Key(KeyEvent { modifiers, code, kind, .. }) if kind != KeyEventKind::Release => {
+                match (modifiers, code) {
+                    (KeyModifiers::CONTROL, KeyCode::Char('q')) => should_exit.set(true),
                     _ => {}
                 }
             }
@@ -30,28 +47,91 @@ fn Mainview(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
     element! {
         View(
             width: width - 1,
-            height,
-            background_color: Color::DarkGrey,
+            height: height - 1,
+            //background_color: Color::DarkGrey,
             border_style: BorderStyle::Single,
             border_color: Color::Grey,
             flex_direction: FlexDirection::Column,
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Start,
+            justify_content: JustifyContent::Start,
         ){
-            Lilview()
-            Text(content: "Press q to quit")
+            NoteEditor()
+            Footer()
         }
     }
 }
 
 #[component]
-fn Lilview(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+fn AppBody(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
     element! {
         View(
-            border_style: BorderStyle::Round,
-            border_color: Color::Blue,
+            flex_grow: 1.0,
+            flex_direction: FlexDirection::Row,
+        ){
+            // shows editor unless ctrl+n opens note list
+            NoteExplorer()
+            NoteEditor()
+        }
+    }
+}
+
+#[component]
+fn NoteEditor(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    let mut note_text = hooks.use_state(|| "".to_string());    
+
+    hooks.use_terminal_events({
+        move |event| match event {
+            TerminalEvent::Key(KeyEvent { code, kind, .. }) if kind != KeyEventKind::Release => {
+                if code == KeyCode::Enter {
+                    let mut ntstring = note_text.to_string();
+                    ntstring.insert(ntstring.len(), '\n');
+                    note_text.set(ntstring);
+                }
+            }
+            _ => {}
+        }
+    });
+
+    element! {
+        View(
+            width: 100pct,
+            height: 100pct,
+            flex_wrap: FlexWrap::Wrap
+        ){
+            TextInput(
+                has_focus: true,
+                value: note_text.to_string(),
+                on_change: move |new_value| note_text.set(new_value),
+            )
+        }
+    }
+}
+
+#[component]
+fn NoteExplorer(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            flex_grow: 0.3,
+        ){
+
+        }
+    }
+}
+
+#[component]
+fn Footer(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            height: 1,
+            border_style: BorderStyle::None,
+            border_color: Color::White,
+            background_color: Color::DarkGrey,
+            flex_direction: FlexDirection::Row,
+            align_items: AlignItems::Start,
+            gap: 3,
         ) {
-            Text(content: "Hello, world!")
+            Text(content: "Ctrl Q: Quit")
+            //Text(content: "Ctrl E: Explore") // or Edit if in explorer
         }
     }
 }
